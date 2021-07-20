5dbe4c43efb1de7a0c6921745e42307143e3ff0d
christian.dismaya <christian.dismaya@growsuper.com>
2021-07-20 12:08:04 +0800

    added a description of each endpoint

--- src/main/kotlin/com/example/test/controller/BookController.kt
+++ src/main/kotlin/com/example/test/controller/BookController.kt
@@ -16,44 +16,51 @@ import org.springframework.web.bind.annotation.*
 class BookController(private val bookService: BookService) {
 
     @GetMapping(value = ["/books"], produces = [MediaType.APPLICATION_JSON_VALUE])
-    @ApiOperation(value = "")
+    @ApiOperation(value = "Get all the book that you created")
     fun getAll(): ResponseEntity<ResponseModel> {
         return bookService.getAllBooks()
     }
 
     @PostMapping(value = ["/book"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ApiOperation(value = "Create a new book to be save on DB")
     fun addBook(@RequestBody request: AddBookModel): ResponseEntity<ResponseModel> {
         return bookService.addBook(request)
     }
 
     @PostMapping(value = ["/books"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ApiOperation(value = "Create a multiple books to be save on DB")
     fun addBooks(@RequestBody request: List<AddBookModel>): ResponseEntity<ResponseModel> {
         return bookService.addBooks(request)
     }
 
     @PutMapping(value = ["/book/{id}/edit"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ApiOperation(value = "Edit the details of the book you wanted by passing the id of the book")
     fun updateBookById(@PathVariable("id") bookId: Int,
                        @RequestBody request: UpdateBookModel): ResponseEntity<ResponseModel> {
         return bookService.editBookById(bookId, request)
     }
 
     @DeleteMapping(value = ["/book/{id}"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ApiOperation(value = "Delete a book by passing id of the book")
     fun deleteBooksById(@PathVariable("id") bookId: Int): ResponseEntity<ResponseModel> {
         return bookService.deleteBookById(bookId)
     }
 
-    @DeleteMapping(value = ["/books"], produces = [])
+    @DeleteMapping(value = ["/books"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ApiOperation(value = "Delete books by passing multiple id of the books")
     fun deleteBooksById(@RequestBody bookIds: List<Int>): ResponseEntity<ResponseModel> {
         return bookService.deleteBooksByIds(bookIds)
     }
 
     @PutMapping(value = ["/book/{id}/assign"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ApiOperation(value = "Assign category for a book by passing the id of the category and book")
     fun assignBookCategoryById(@PathVariable("id") bookId: Int,
                                @RequestParam categoryId: Int): ResponseEntity<ResponseModel> {
         return bookService.assignBookCategoryById(bookId, categoryId)
     }
 
     @PutMapping(value = ["/book/{id}/remove"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ApiOperation(value = "Remove category for a book by passing the id of the category and book")
     fun removeBookCategoryById(@PathVariable("id") bookId: Int,
                                @RequestParam categoryId: Int): ResponseEntity<ResponseModel> {
         return bookService.removeBookCategoryById(bookId, categoryId)
--- src/main/kotlin/com/example/test/controller/CategoryController.kt
+++ src/main/kotlin/com/example/test/controller/CategoryController.kt
@@ -2,6 +2,8 @@ package com.example.test.controller
 
 import com.example.test.models.ResponseModel
 import com.example.test.service.CategoryService
+import io.swagger.annotations.ApiOperation
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -10,17 +12,20 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/api")
 class CategoryController(private val categoryService: CategoryService) {
 
-    @PostMapping("/category")
+    @PostMapping(value = ["/category"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ApiOperation(value = "Create a new category to be save on DB")
     fun addCategory(@RequestParam category: String): ResponseEntity<ResponseModel> {
         return categoryService.addCategory(category)
     }
 
-    @PostMapping("/categories")
+    @PostMapping(value = ["/categories"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ApiOperation(value = "Create a multiple category to be save on DB")
     fun addCategories(@RequestBody categories: List<String>): ResponseEntity<ResponseModel> {
         return categoryService.addCategories(categories)
     }
 
-    @GetMapping("/categories")
+    @GetMapping(value = ["/categories"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ApiOperation(value = "Get all the category that you created")
     fun getAllCategories(): ResponseEntity<ResponseModel> {
         return categoryService.getAllCategories()
     }
